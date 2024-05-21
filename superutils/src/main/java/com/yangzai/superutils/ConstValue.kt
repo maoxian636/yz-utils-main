8fbf23afa2aa3b1224d8bd84fb751d111d28e024
maoxian636 <2572392156@qq.com>
2024-05-21 13:04:07 +0800

    11

--- superutils/src/main/java/com/yangzai/superutils/ConstValue.kt
+++
@@ -1,7 +0,0 @@
-package com.yangzai.superutils
-
-/**
- *@author 许阳
- *2024/3/7 14:42
- **/
-const val TAG = "xasqwsw"
--- superutils/src/main/java/com/yangzai/superutils/Test.kt
+++
@@ -1,7 +0,0 @@
-package com.yangzai.superutils
-
-class Test {
-    val A =100
-    val B = 200
-
-}
